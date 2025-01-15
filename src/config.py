0c340ab9fa3ff0ab0dab8f18ffcefc45a4e35b93
AlexanderKhmylov <a.a.hm@yandex.ru>
2025-01-15 14:01:22 +0300

    add settings

--- src/config.py
+++ src/config.py
@@ -1,15 +1,25 @@
-from typing import Optional
-
-from pydantic import EmailStr
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     app_title: str = "Tabit"
     description: str = "Tabit platform"
-    database_url: str = "postgresql+asyncpg://admin:password@localhost/db_tabit"
-    first_superuser_email: Optional[EmailStr] = None
-    first_superuser_password: Optional[str] = None
+
+    db_type: str = "postgres"
+    db_api: str = "asyncpg"
+    db_user: str = "username"
+    db_password: SecretStr = "password"
+    db_host: str = "localhost"
+    db_name: str = "db_tabit"
+
+    @property
+    def database_url(self):
+        return (
+            f"{self.db_type}+{self.db_api}://"
+            f"{self.db_user}:{self.db_password}@{self.db_host}"
+            f"/{self.db_name}"
+        )
 
     class Config:
         env_file = ".env"
