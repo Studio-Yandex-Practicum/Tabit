8f88f0caabfcf1168c2950b069c9fe3de38794c6
Anton Stremousov <stremousoff@gmail.com>
2025-01-16 00:48:07 +0300

    remove literals

--- src/main.py
+++ src/main.py
@@ -3,8 +3,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.api.v1.routers import main_router
+from src.config import Settings
 
-app_v1 = FastAPI(title="Tabit", description="Tabit platform", version="1.0.0")
+app_v1 = FastAPI(
+    title=Settings.app_title,
+    description=Settings.description,
+    version=Settings.version
+)
 
 app_v1.include_router(main_router)
 
