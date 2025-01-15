4bb3b647950b75a3e593590cb0c829323e04d6ee
AlexanderKhmylov <97311102+AlexanderKhmylov@users.noreply.github.com>
2025-01-15 15:49:24 +0300

    add settings (#15)
    Тикет вашего PR (если есть):
        ___ваш тикет___
    
    Когда создаёте PR, убедитесь, что:
    
    - [ ] синхронизировали изменения кода с [ER-диаграммой базы
    данных](https://app.erdlab.io/designer/schema/1736745715-tabit) связей
    моделей
    - [ ] убедились, что модель OpenAPI проверена (в Swagger отображаются
    изменения)
    - [ ] тесты все пройдены и нет конфликтов
    - [ ] созданы необходимые миграции
    
    Когда ваш PR смержен:
    
    - [ ] если у вас были изменения в API, сообщите об этом команде Фронта,
    им это важно знать!

--- src/config.py
+++ src/config.py
@@ -1,1 +1,28 @@
-# global configs
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = "Tabit"
+    description: str = "Tabit platform"
+
+    db_type: str = "postgres"
+    db_api: str = "asyncpg"
+    db_user: str = "username"
+    db_password: SecretStr = "password"
+    db_host: str = "localhost"
+    db_name: str = "db_tabit"
+
+    @property
+    def database_url(self):
+        return (
+            f"{self.db_type}+{self.db_api}://"
+            f"{self.db_user}:{self.db_password}@{self.db_host}"
+            f"/{self.db_name}"
+        )
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
