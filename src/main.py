276a6b6553fe9df248cb1477bba848caace57d65
Anton Stremousov <stremousoff@gmail.com>
2025-01-16 00:06:26 +0300

    add version

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 
 from src.api.v1.routers import main_router
 
-app = FastAPI(title="Tabit", description="Tabit platform", version="1.0.0")
+app_v1 = FastAPI(title="Tabit", description="Tabit platform", version="1.0.0")
 
-app.include_router(main_router)
+app_v1.include_router(main_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app_v1", reload=True)
