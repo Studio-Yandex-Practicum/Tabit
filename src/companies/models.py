942651c239f70695936329a63d2370672978d7b8
Maksim Karaskin <mac7sim@gmail.com>
2025-01-16 11:00:59 +0300

    Company

--- src/companies/models.py
+++ src/companies/models.py
@@ -1,1 +1,37 @@
-# db models
+from datetime import date
+
+from sqlalchemy import ForeignKey, Boolean
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from src.database import BaseTabitModel, str_uniq
+
+
+class Company(BaseTabitModel):
+    name: Mapped[str_uniq]
+    description: Mapped[str]
+    license_id: Mapped[int] = mapped_column(ForeignKey("license.id"))
+    employees = relationship(
+        'UserTabit', secondary='company_usertabit', back_populates='company'
+    )
+    employees: Mapped["UserTabit"] = relationship(
+        "UserTabit", back_populates="usertabits"
+    )
+    departments: Mapped["Department"] = relationship(
+        "Department", back_populates="companies"
+    )
+    max_admins_count = Mapped[int]
+    max_employees_count = Mapped[int]
+    start_license_time: Mapped[date]
+    end_license_time: Mapped[date]
+
+    demo_mode: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    test_cycle_duration = Mapped[int]
+    day_for_testing = Mapped[int]
+
+
+class Department(BaseTabitModel):
+    pass
+
+
+class UserTabit(BaseTabitModel):
+    pass
--- src/database.py
+++ src/database.py
@@ -1,1 +1,15 @@
 # db connection related stuff
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import mapped_column
+
+# настройка аннотаций
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[
+    datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)
+]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
