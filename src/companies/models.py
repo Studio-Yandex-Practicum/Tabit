9d8d34c8c07b0d6e1550cbefdbab0d0caba4360b
Maksim Karaskin <mac7sim@gmail.com>
2025-01-16 14:38:47 +0300

    Нужно перенести UserTabit model

--- src/companies/models.py
+++ src/companies/models.py
@@ -3,21 +3,20 @@ from datetime import date
 from sqlalchemy import ForeignKey, Boolean
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
-from src.database import BaseTabitModel, str_uniq
+from src.database import BaseTabitModel
+from . import BaseUser, UserTags
 
 
 class Company(BaseTabitModel):
-    name: Mapped[str_uniq]
+    """Модель компании."""
+    company_name: Mapped[str] = mapped_column(unique=True)
     description: Mapped[str]
-    license_id: Mapped[int] = mapped_column(ForeignKey("license.id"))
-    employees = relationship(
-        'UserTabit', secondary='company_usertabit', back_populates='company'
-    )
-    employees: Mapped["UserTabit"] = relationship(
-        "UserTabit", back_populates="usertabits"
+    license_id: Mapped[int] = mapped_column(ForeignKey('license.id'))
+    employees: Mapped['UserTabit'] = relationship(
+        'UserTabit', back_populates='user_tabits'
     )
     departments: Mapped["Department"] = relationship(
-        "Department", back_populates="companies"
+        "Department", back_populates='companies'
     )
     max_admins_count = Mapped[int]
     max_employees_count = Mapped[int]
@@ -30,8 +29,21 @@ class Company(BaseTabitModel):
 
 
 class Department(BaseTabitModel):
-    pass
+    """Модель отдела."""
+    department_name: Mapped[str] = mapped_column(unique=True)
+    supervisor_id: Mapped[int] = mapped_column(ForeignKey('user_tabit.uuid'))
 
 
-class UserTabit(BaseTabitModel):
-    pass
+class UserTabit(BaseUser):
+    """Модель сотрудника."""
+    role: Mapped[str]
+    telegram_username: Mapped[str] = mapped_column(unique=True)
+    company: Mapped[int] = mapped_column(ForeignKey('company.id'))
+    start_date_employment: Mapped[date]
+    end_date_employment: Mapped[date]
+    birthday: Mapped[date]
+    last_department: Mapped[int] = mapped_column(ForeignKey('department.id'))
+    employee_position: Mapped[str]
+    user_tags: Mapped['UserTags'] = relationship(
+        'UserTags', back_populates='user_tags'
+    )
