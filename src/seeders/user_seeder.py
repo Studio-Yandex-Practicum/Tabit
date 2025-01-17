8271677bfca40882596c63e58eb161333fe9f447
Maksim Berezin <mms2000@mail.ru>
2025-01-17 10:57:57 +0300

    Добавил UserSeeder и базовый сидер с использованием асинхронных сессий

---
+++ src/seeders/user_seeder.py
@@ -0,0 +1,34 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.companies.models import User
+from src.tabit_management.constants import FAKER_USER_COUNT
+from src.utils.faker_instance import fake
+from .base_seeder import BaseSeeder
+
+
+class UserSeeder(BaseSeeder):
+    """
+    Асинхронный сидер для генерации тестовых пользователей.
+    """
+
+    def __init__(self, count=FAKER_USER_COUNT):
+        """
+        Инициализация сидера пользователей.
+        :param count: количество пользователей для генерации
+        """
+        super().__init__(count)
+
+    async def run(self, session: AsyncSession):
+        """
+        Генерация и добавление пользователей в базу данных.
+        :param session: асинхронная сессия SQLAlchemy
+        """
+        # TODO: когда будет модель, поправлю поля
+        users = [
+            User(username=fake.user_name(), email=fake.email())
+            for _ in range(self.count)
+        ]
+        session.add_all(users)
+        await session.commit()
+
+        # TODO: пока print, но нужно логирование
+        print(f'Создано {self.count} пользователей.')
