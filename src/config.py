6b703e26a5b4ffe5f0b372635345d47bb3069575
Lev Akchurin <levisserena@yandex.ru>
2025-01-19 14:12:54 +0400

    container postgres

--- src/config.py
+++ src/config.py
@@ -8,18 +8,18 @@ class Settings(BaseSettings):
     version: str = '1.0.0'
     db_type: str = 'postgres'
     db_api: str = 'asyncpg'
+    db_host: str = 'localhost'
     postgres_user: str = 'username'
     postgres_password: SecretStr = 'password'
     postgres_db: str = 'db_tabit'
     port_bd_postgres: str = '5433'
-    db_host: str = 'localhost'
-
 
     @property
     def database_url(self):
         return (
             f'{self.db_type}+{self.db_api}://'
-            f'{self.postgres_user}:{self.postgres_password}@{self.db_host}'
+            f'{self.postgres_user}:{self.postgres_password}@'
+            f'{self.db_host}:{self.port_bd_postgres}'
             f'/{self.postgres_db}'
         )
 
