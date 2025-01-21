72435802f2dfb35bcd0a9811ff9fe7aecf3be77b
Maksim Berezin <155676731+mms2000@users.noreply.github.com>
2025-01-21 18:46:09 +0300

    feature/#44-crud-employees (#50)
    Добавлены моковые эндпоинты CRUD для employees.
    - Добавлены базовые маршруты (GET, POST, PATCH, DELETE).
    - TODO: Подключить базу данных и валидацию.

--- src/api/v1/endpoints/department.py
+++ src/api/v1/endpoints/department.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.database import get_async_session
+
+from src.database.db_depends import get_async_session
 
 router = APIRouter()
 
--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from src.api.v1.endpoints import main_page_router, department_router
+from src.api.v1.endpoints import main_page_router, department_router, user_router
 
 main_router = APIRouter()
 
 
 main_router.include_router(main_page_router, prefix='', tags=['Main page'])
 
-main_router.include_router(department_router, prefix='/department', tags=['companies'])
+main_router.include_router(department_router, prefix='/department', tags=['Companies'])
+
+main_router.include_router(user_router, prefix='/users', tags=['Companies'])
