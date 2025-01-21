7de2083fd5153ea219f2e2085e8429ba03152cb4
Maksim Berezin <mms2000@mail.ru>
2025-01-21 11:42:18 +0300

    Добавлены моковые эндпоинты CRUD для users (поравил с соотрудников)

--- src/api/v1/endpoints/__init__.py
+++ src/api/v1/endpoints/__init__.py
@@ -1,9 +1,9 @@
 from .endpoint import router as main_page_router  # noqa
 from .department import router as department_router  # noqa
-from .employee import router as employee_router  # noqa
+from .users import router as user_router  # noqa
 
 __all__ = [
     'main_page_router',
     'department_router',
-    'employee_router',
+    'user_router',
 ]
