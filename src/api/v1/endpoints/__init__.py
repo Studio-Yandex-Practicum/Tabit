2aa551b6ae0d04993aae928b6157974bc863c2fd
Anton Stremousov <stremousoff@gmail.com>
2025-01-22 16:18:00 +0300

    34-добавить-erd-в-репозиторий

--- src/api/v1/endpoints/__init__.py
+++ src/api/v1/endpoints/__init__.py
@@ -1,9 +1,11 @@
 from .endpoint import router as main_page_router  # noqa
 from .department import router as department_router  # noqa
 from .users import router as user_router  # noqa
+from .survey import router as survey_router  # noqa
 
 __all__ = [
     'main_page_router',
     'department_router',
     'user_router',
+    'survey_router',
 ]
--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.api.v1.endpoints import main_page_router, department_router, user_router
+from src.api.v1.endpoints import main_page_router, department_router, user_router, survey_router
 
 main_router = APIRouter()
 
@@ -10,3 +10,5 @@ main_router.include_router(main_page_router, prefix='', tags=['Main page'])
 main_router.include_router(department_router, prefix='/department', tags=['Companies'])
 
 main_router.include_router(user_router, prefix='/users', tags=['Companies'])
+
+main_router.include_router(survey_router, prefix='/survey', tags=['Surveys'])
