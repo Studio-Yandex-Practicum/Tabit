3c34ba1ae0d908061c70b6cf63b52a0252e072dd
Lev Akchurin <levisserena@yandex.ru>
2025-01-22 21:56:31 +0400

    router_admin-and-company: conflict resolution

--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from src.api.v1.endpoints import (
-    main_page_router,
+    companies_router,
     department_router,
     problem_router,
     user_router,
@@ -9,9 +9,7 @@ from src.api.v1.endpoints import (
 
 main_router = APIRouter()
 
-main_router.include_router(main_page_router, prefix='', tags=['Main page'])
 main_router.include_router(companies_router, prefix='/companies', tags=['Companies'])
 main_router.include_router(department_router, prefix='/department', tags=['Companies'])
 main_router.include_router(user_router, prefix='/users', tags=['Companies'])
-
 main_router.include_router(problem_router, prefix='/problem', tags=['Problems'])
