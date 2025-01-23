de2f306bef6e8706e72c3693a957a64b55991c5c
Konstantin Raikhert <raikhert13@gmail.com>
2025-01-23 12:28:24 +0300

    some fixes

--- src/companies/schemas.py
+++ src/companies/schemas.py
@@ -1,1 +1,21 @@
-# pydantic models
+from pydantic import BaseModel
+
+
+class CompanyCreate(BaseModel):
+    pass
+
+
+class CompanyDB(BaseModel):
+    pass
+
+
+class CompanyDBForUser(BaseModel):
+    pass
+
+
+class CompanyUpdate(BaseModel):
+    pass
+
+
+class CompanyUpdateForUser(BaseModel):
+    pass
