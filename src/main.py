c889c1f03fbee36ada1e77505361c3c02a97aa17
AlexanderKhmylov <a.a.hm@yandex.ru>
2025-01-23 03:40:49 +0300

    add logger functionality, code refactored

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ from fastapi import FastAPI
 
 from src.api.v1.routers import main_router
 from src.config import settings
-from .logger import LoggingMiddleware
+from src.logger import LoggingMiddleware, logger
 
 
 app_v1 = FastAPI(
     title=settings.app_title, description=settings.description, version=settings.version
 )
-app_v1.middleware('http')(LoggingMiddleware())
+app_v1.middleware('http')(
+    LoggingMiddleware()
+)  # Add logging requests feature as middleware
 app_v1.include_router(main_router)
 
 if __name__ == '__main__':
+    logger.info('Starting uvicorn server...')
     uvicorn.run('main:app_v1', reload=True)
