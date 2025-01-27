937a4050d7397382b127535aa8960646d82a3bad
Lev Akchurin <levisserena@yandex.ru>
2025-01-27 21:32:25 +0400

    endpoints-company_user_auth: pilot endpoints auth employee

--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from src.api.v1.endpoints import (
+    auth_employees,
     companies_router,
     department_router,
     problem_router,
@@ -10,7 +11,7 @@ from src.api.v1.endpoints import (
     department_reports_router,
 )
 
-main_router = APIRouter()
+main_router = APIRouter(prefix='/api/v1')
 
 main_router.include_router(companies_router, prefix='/companies', tags=['Companies'])
 main_router.include_router(department_router, prefix='/department', tags=['Departments'])
@@ -19,3 +20,5 @@ main_router.include_router(problem_router, prefix='/problem', tags=['Problems'])
 # main_router.include_router(superuser_router, prefix='/superuser', tags=['Superuser'])
 # main_router.include_router(admin_router, prefix='/admin', tags=['Admin'])
 main_router.include_router(department_reports_router, prefix='/survey', tags=['Survey Reports'])
+
+main_router.include_router(auth_employees, prefix='/auth', tags=['Auth Employees'])
