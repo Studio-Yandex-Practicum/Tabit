40149bc0b65d66a2c65fd3e0f49551330e33f12b
Anton Stremousov <stremousoff@gmail.com>
2025-01-28 19:59:00 +0300

    add problems endpoint

--- src/api/v1/endpoints/__init__.py
+++ src/api/v1/endpoints/__init__.py
@@ -2,8 +2,8 @@ from src.api.v1.endpoints.company_user_auth import router as auth_employees
 from .companies import router as companies_router
 from .department import router as department_router
 from .department_report import router as department_reports_router
-from .problem import router as problem_router
 from .users import router as user_router
+from .problems import router as problems_router
 # from .endpoint import main_router, superuser_router, admin_router  # noqa: F401
 
 
@@ -13,7 +13,6 @@ __all__ = [
     'department_router',
     'companies_router',
     'user_router',
-    'problem_router',
     'superuser_router',
     'admin_router',
     'department_reports_router',
--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -4,7 +4,7 @@ from src.api.v1.endpoints import (
     auth_employees,
     companies_router,
     department_router,
-    problem_router,
+    problems_router,
     user_router,
     # superuser_router,
     # admin_router,
@@ -16,9 +16,9 @@ main_router = APIRouter(prefix='/api/v1')
 main_router.include_router(companies_router, prefix='/companies', tags=['Companies'])
 main_router.include_router(department_router, prefix='/department', tags=['Departments'])
 main_router.include_router(user_router, prefix='/users', tags=['Tabit Users'])
-main_router.include_router(problem_router, prefix='/problem', tags=['Problems'])
+main_router.include_router(problems_router, prefix='', tags=['Problems'])
 # main_router.include_router(superuser_router, prefix='/superuser', tags=['Superuser'])
-# main_router.include_router(admin_router, prefix='/admin', tags=['Admin'])
+# main_router.include_router(admin_router, prefi='/admin', tags=['Admin'])
 main_router.include_router(department_reports_router, prefix='/survey', tags=['Survey Reports'])
 
 main_router.include_router(auth_employees, prefix='/auth', tags=['Auth Employees'])
