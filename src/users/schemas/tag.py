370a50749b7f8723ba2171d6dde8d7d8f5ce3486
levisserena <153556774+levisserena@users.noreply.github.com>
2025-01-31 09:21:19 +0400

    add_schemas_all_users: add schemas for admin, user, tag_user, and bug… (#122)
    Добавил схемы к АдминамТабита, пользователям, тэгам пользователям, а так
    же поправил схемы типов лицензий.
    Новые схемы к эндпоинтам не подключал.

---
+++ src/users/schemas/tag.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from src.constants import LENGTH_NAME_USER, MIN_LENGTH_NAME
+from src.users.constants import title_company_id_tag, title_name_tag
+
+
+class TagUserUpdateSchema(BaseModel):
+    """Схема для частичного изменения тэгов пользователей."""
+
+    name: str = Field(
+        ...,
+        min_length=MIN_LENGTH_NAME,
+        max_length=LENGTH_NAME_USER,
+        title=title_name_tag,
+    )
+
+
+class TagUserCreateSchema(TagUserUpdateSchema):
+    """Схема для создания тэгов пользователей."""
+
+    company_id: int = Field(
+        ...,
+        title=title_company_id_tag,
+    )
+
+
+class TagUserResponseSchema(BaseModel):
+    """Схема тэгов пользователей для ответов."""
+
+    id: int
+    name: str
+    company_id: int
+    created_at: date
+    updated_at: date
+
+    model_config = ConfigDict(from_attributes=True)
