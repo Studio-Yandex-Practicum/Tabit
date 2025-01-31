4d373d6ecb2ebe617fccf8ffb72d92aa10ae0a30
Anton Stremousov <stremousoff@gmail.com>
2025-01-31 08:01:23 +0300

    110 create pydantic model for files (#118)

---
+++ src/problems/schemas/file.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+
+from pydantic import BaseModel, field_validator
+
+ALLOWED_FILE_EXTENSIONS = ('.pdf', '.doc', '.docx')
+ALLOWED_SIZE_FILE = 10 * 1024 * 1024 #10Mb
+
+
+class BaseFileSchema(BaseModel):
+    file_path: str
+    entity_id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+    @field_validator('file_path')
+    def validate_file(cls, path: str) -> str:
+        match path:
+            case _ if not any(
+                    path.endswith(ext) for ext in ALLOWED_FILE_EXTENSIONS
+            ):
+                raise ValueError(
+                    f'Файл должен иметь расширение: '
+                    f'{", ".join(ALLOWED_FILE_EXTENSIONS)}'
+                )
+            case _ if os.path.getsize(path) > ALLOWED_SIZE_FILE:
+                raise ValueError(
+                    f'Размер файла не должен превышать '
+                    f'{ALLOWED_SIZE_FILE / (1024 * 1024)} МБ'
+                )
+            case _ if not os.path.exists(path):
+                raise ValueError(f'Файла по адресу {path} не существует')
+            case _:
+                return path
+
+class FileCreateSchema(BaseFileSchema):
+    pass
+
+class FileUpdateSchema(BaseFileSchema):
+    pass
+
+class FileResponseSchema(BaseFileSchema):
+    id: int
