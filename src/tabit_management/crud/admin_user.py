53b3a812406631f92c3d90cdba553d8546727e1c
AndreiAgeev <ageevandrei1996@gmail.com>
2025-02-08 18:53:18 +0300

    fixed get_by_email_or_number method in CRUDAdminUser

--- src/tabit_management/crud/admin_user.py
+++ src/tabit_management/crud/admin_user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy import or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,13 +10,19 @@ from src.tabit_management.models import TabitAdminUser
 class CRUDAdminUser(CRUDBase):
     """CRUD операций для моделей администраторов сервиса Табит."""
 
-    async def get_by_email_or_number(self, email: str, number: str, session: AsyncSession):
-        """Получает объект пользователя по email или phone_number."""
-        admin_user = await session.execute(
-            select(self.model).where(
-                or_(self.model.email == email, self.model.phone_number == number)
+    async def get_by_email_or_number(
+        self, email: str, number: Optional[str], session: AsyncSession
+    ):
+        """Получает объект пользователя по email и phone_number или только по email."""
+
+        if number:
+            admin_user = await session.execute(
+                select(self.model).where(
+                    or_(self.model.email == email, self.model.phone_number == number)
+                )
             )
-        )
+        else:
+            admin_user = await session.execute(select(self.model).where(self.model.email == email))
         return admin_user.scalars().first()
 
 
