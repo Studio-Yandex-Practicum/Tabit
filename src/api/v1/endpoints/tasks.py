79da2943ba9214404e7d78890694a78d3a60b381
Руслан Касымов <marco.markovich@yandex.ru>
2025-02-10 20:31:52 +0300

    Таска 136 и 137 (Реализация crud и pydantic схем для эндпоинтов). 136 таску сделал, проверил через доку, (все операции доступны), с таской 137 не получается реализовать создание)<убрал реализацию 138>

--- src/api/v1/endpoints/tasks.py
+++ src/api/v1/endpoints/tasks.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db_depends import get_async_session
-from src.problems.crud.tasks import task_crud
 from src.problems.schemas.task import TaskResponseSchema, TaskCreateSchema, TaskUpdateSchema
-# from src.problems.models.enums import StatusTask
+from src.problems.models.enums import StatusTask
 
 router = APIRouter()
 
@@ -20,30 +19,28 @@ async def get_tasks(
     company_slug: str, problem_id: int, session: AsyncSession = Depends(get_async_session)
 ):
     """Возвращает информацию о всех задачах проблемы"""
-    # # TODO: Реализовать получение задач для проблемы из БД
-    # task_schema = TaskResponseSchema(
-    #     **{
-    #         'id': 1,
-    #         'name': f'Задача №1 у компании {company_slug}',
-    #         'problem_id': problem_id,
-    #         'description': 'Описание задачи #1',
-    #         'date_completion': '2030-01-01',
-    #         'owner_id': '3fa85f64-5717-4562-b3fc-2c963f234331',
-    #         'executor': [
-    #             '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #             '3fa85f64-5717-4562-b3fc-2c9633333fa1',
-    #         ],
-    #         'status': StatusTask.NEW,
-    #         'transfer_counter': 0,
-    #         'file': [
-    #             '/file1.jpg',
-    #             '/file2.pdf',
-    #         ],
-    #     }
-    # )
-    # return [task_schema] * 2
-    filters = {'problem_id': problem_id}
-    return await task_crud.get_multi(session, filters=filters)
+    # TODO: Реализовать получение задач для проблемы из БД
+    task_schema = TaskResponseSchema(
+        **{
+            'id': 1,
+            'name': f'Задача №1 у компании {company_slug}',
+            'problem_id': problem_id,
+            'description': 'Описание задачи #1',
+            'date_completion': '2030-01-01',
+            'owner_id': '3fa85f64-5717-4562-b3fc-2c963f234331',
+            'executor': [
+                '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+                '3fa85f64-5717-4562-b3fc-2c9633333fa1',
+            ],
+            'status': StatusTask.NEW,
+            'transfer_counter': 0,
+            'file': [
+                '/file1.jpg',
+                '/file2.pdf',
+            ],
+        }
+    )
+    return [task_schema] * 2
 
 
 @router.post(
@@ -60,17 +57,16 @@ async def create_task(
     session: AsyncSession = Depends(get_async_session),
 ):
     """Создает новую задачу"""
-    # # TODO: Реализовать создание задачи в БД
-    # task_schema = TaskResponseSchema(
-    #     id=123,
-    #     problem_id=problem_id,
-    #     owner_id='3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #     status=StatusTask.NEW,
-    #     transfer_counter=0,
-    #     **task.model_dump(exclude_none=True),
-    # )
-    # return task_schema
-    return await task_crud.create(session, task)
+    # TODO: Реализовать создание задачи в БД
+    task_schema = TaskResponseSchema(
+        id=123,
+        problem_id=problem_id,
+        owner_id='3fa85f64-5717-4562-b3fc-2c963f66afa1',
+        status=StatusTask.NEW,
+        transfer_counter=0,
+        **task.model_dump(exclude_none=True),
+    )
+    return task_schema
 
 
 @router.get(
@@ -87,23 +83,22 @@ async def get_task(
     session: AsyncSession = Depends(get_async_session),
 ):
     """Получает информацию о задаче"""
-    # # TODO: Реализовать получение задачи из БД
-    # task_from_db = {
-    #     'id': task_id,
-    #     'name': f'Задача №1 у компании {company_slug}',
-    #     'problem_id': problem_id,
-    #     'description': 'Описание задачи #1',
-    #     'date_completion': '2030-01-01',
-    #     'owner_id': '3fa85f64-5717-4562-b3fc-2c963f234331',
-    #     'executor': [
-    #         '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #         '3fa85f64-5717-4562-b3fc-2c9633333fa1',
-    #     ],
-    #     'status': StatusTask.NEW,
-    #     'transfer_counter': 0,
-    # }
-    # return task_from_db
-    return await task_crud.get_or_404(session, task_id)
+    # TODO: Реализовать получение задачи из БД
+    task_from_db = {
+        'id': task_id,
+        'name': f'Задача №1 у компании {company_slug}',
+        'problem_id': problem_id,
+        'description': 'Описание задачи #1',
+        'date_completion': '2030-01-01',
+        'owner_id': '3fa85f64-5717-4562-b3fc-2c963f234331',
+        'executor': [
+            '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+            '3fa85f64-5717-4562-b3fc-2c9633333fa1',
+        ],
+        'status': StatusTask.NEW,
+        'transfer_counter': 0,
+    }
+    return task_from_db
 
 
 @router.patch(
@@ -121,27 +116,26 @@ async def update_task(
     session: AsyncSession = Depends(get_async_session),
 ):
     """Обновляет информацию задачи"""
-    # # TODO: Реализовать получение задачи из БД
-    # task_from_db = {
-    #     'id': task_id,
-    #     'name': f'Задача №1 у компании {company_slug}',
-    #     'problem_id': problem_id,
-    #     'description': 'Описание задачи #1',
-    #     'date_completion': '2030-01-01',
-    #     'owner_id': '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #     'executor': [
-    #         '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #         '3fa85f64-5717-4562-b3fc-2c9633333fa1',
-    #     ],
-    #     'status': StatusTask.NEW,
-    #     'transfer_counter': 0,
-    # }
-    # # TODO: Реализовать обновление задачи в БД
-    # task_to_update = task.model_dump(exclude_none=True)
-    # task_from_db.update(task_to_update)
-    # task_schema = TaskResponseSchema(**task_from_db)
-    # return task_schema
-    return await task_crud.update(session, task_id, task)
+    # TODO: Реализовать получение задачи из БД
+    task_from_db = {
+        'id': task_id,
+        'name': f'Задача №1 у компании {company_slug}',
+        'problem_id': problem_id,
+        'description': 'Описание задачи #1',
+        'date_completion': '2030-01-01',
+        'owner_id': '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+        'executor': [
+            '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+            '3fa85f64-5717-4562-b3fc-2c9633333fa1',
+        ],
+        'status': StatusTask.NEW,
+        'transfer_counter': 0,
+    }
+    # TODO: Реализовать обновление задачи в БД
+    task_to_update = task.model_dump(exclude_none=True)
+    task_from_db.update(task_to_update)
+    task_schema = TaskResponseSchema(**task_from_db)
+    return task_schema
 
 
 @router.delete(
@@ -158,20 +152,19 @@ async def delete_task(
     session: AsyncSession = Depends(get_async_session),
 ):
     """Удаляет задачу"""
-    # # TODO: Реализовать удаление задачи из БД
-    # task_from_db = {
-    #     'id': task_id,
-    #     'name': f'Задача №1 у компании {company_slug}',
-    #     'problem_id': problem_id,
-    #     'description': 'Описание задачи #1',
-    #     'date_completion': '2030-01-01',
-    #     'owner_id': '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #     'executor': [
-    #         '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-    #         '3fa85f64-5717-4562-b3fc-2c9633333fa1',
-    #     ],
-    #     'status': StatusTask.NEW,
-    #     'transfer_counter': 0,
-    # }
-    # return TaskResponseSchema(**task_from_db)
-    return await task_crud.remove(session, task_id)
+    # TODO: Реализовать удаление задачи из БД
+    task_from_db = {
+        'id': task_id,
+        'name': f'Задача №1 у компании {company_slug}',
+        'problem_id': problem_id,
+        'description': 'Описание задачи #1',
+        'date_completion': '2030-01-01',
+        'owner_id': '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+        'executor': [
+            '3fa85f64-5717-4562-b3fc-2c963f66afa1',
+            '3fa85f64-5717-4562-b3fc-2c9633333fa1',
+        ],
+        'status': StatusTask.NEW,
+        'transfer_counter': 0,
+    }
+    return TaskResponseSchema(**task_from_db)
--- src/problems/crud/__init__.py
+++ src/problems/crud/__init__.py
@@ -1,10 +1,8 @@
 from .problems import problem_crud
-from .tasks import task_crud
 from .meeting import meeting_crud
 
 
 __all__ = [
     'problem_crud',
-    'task_crud',
     'meeting_crud',
 ]
