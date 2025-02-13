761f9fab75bf0cc2a2bd4933df27b967312304ac
AndreiAgeev <ageevandrei1996@gmail.com>
2025-02-13 14:02:00 +0300

    add authentication for employees

--- src/api/v1/endpoints/company_user_auth.py
+++ src/api/v1/endpoints/company_user_auth.py
@@ -1,45 +1,81 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from http import HTTPStatus
 
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_users import BaseUserManager, models
+from fastapi_users.authentication import Strategy
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.requests import Request
+
+from src.api.v1.auth.dependencies import get_current_user_token
+from src.api.v1.auth.jwt import jwt_auth_backend
+from src.api.v1.auth.managers import get_user_manager
 from src.database.db_depends import get_async_session
 
 router = APIRouter()
 
 
-@router.get(
-    '/',
-)
-async def get_employees(session: AsyncSession = Depends(get_async_session)):
-    """Возвращает список сотрудников."""
+# @router.get(
+#     '/',
+# )
+# async def get_employees(session: AsyncSession = Depends(get_async_session)):
+#     """Возвращает список сотрудников."""
 
-    return {
-        'employees': [
-            'employee_1',
-            'employee_2',
-            'employee_3',
-            '...',
-            'employee_n',
-        ]
-    }
+#     return {
+#         'employees': [
+#             'employee_1',
+#             'employee_2',
+#             'employee_3',
+#             '...',
+#             'employee_n',
+#         ]
+#     }
+
+
+# Временное (или нет) решение по аутентификации обычных пользователей.
+# Нужно для тестов эндпоинтов сотурдников компаний. Два эндпоинта - login и logout.
+# Login возвращает в кач-ве ответа токен. Дальше токен стоит использовать в Postman,
+# запрос передать в форме x-www-form-urlencoded с ключами username и password.
+# В кач-ве username используется email.
 
 
 @router.post(
     '/login',
+    summary='Авторизация для сотрудников',
 )
-async def login_employee(session: AsyncSession = Depends(get_async_session)):
-    """Авторизация пользователя на сервисе."""
+async def login(
+    request: Request,
+    credentials: OAuth2PasswordRequestForm = Depends(),
+    user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    strategy: Strategy[models.UP, models.ID] = Depends(jwt_auth_backend.get_strategy),
+):
+    """
+    Авторизация для сотрудников компаний.
+    """
+    user = await user_manager.authenticate(credentials)
+    if user is None or not user.is_active:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Неверные учетные данные для входа в систему',
+        )
+    response = await jwt_auth_backend.login(strategy, user)
+    await user_manager.on_after_login(user, request, response)
+    return response
 
-    return {
-        'access_token': 'секретный секрет',
-        'refresh_token': 'засекреченный засекрет',
-    }
 
-
-@router.post('/logout')
-async def logout_employee(session: AsyncSession = Depends(get_async_session)):
-    """Выход из сервиса пользователя."""
-
-    return {}
+@router.post(
+    '/logout',
+    summary='Запрос для разлогирования',
+)
+async def logout(
+    user_token: tuple[models.UP, str] = Depends(get_current_user_token),
+    strategy: Strategy[models.UP, models.ID] = Depends(jwt_auth_backend.get_strategy),
+):
+    """
+    Выход из системы для сотрудников компаний.
+    """
+    user, token = user_token
+    return await jwt_auth_backend.logout(strategy, user, token)
 
 
 @router.post('/refresh-token')
