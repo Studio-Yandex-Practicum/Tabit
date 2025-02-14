8d59399a351f321c55293d6069b6b7638b54c759
EgorGorkovoj <158060815+EgorGorkovoj@users.noreply.github.com>
2025-02-14 18:37:54 +0300

    add CRUD, Schemas, endpoints for src/api/v1/endpoints/company_user.py (#149)
    Тикет вашего PR (если есть):
        ___ваш тикет___
    
    Когда создаёте PR, убедитесь, что:
    
    - [x] синхронизировали изменения кода с [ER-диаграммой базы
    данных](https://app.erdlab.io/designer/schema/1736745715-tabit) связей
    моделей
    - [x] убедились, что модель OpenAPI проверена (в Swagger отображаются
    изменения)
    - [x] тесты все пройдены и нет конфликтов
    - [x] созданы необходимые миграции
    
    Когда ваш PR смержен:
    
    - [ ] если у вас были изменения в API, сообщите об этом команде Фронта,
    им это важно знать!

--- src/companies/schemas/company.py
+++ src/companies/schemas/company.py
@@ -1,12 +1,29 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Self
 
-from pydantic import BaseModel, ConfigDict, Field, model_validator
-from typing_extensions import Self
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, model_validator
 
-from src.companies.constants import TEST_ERROR_LICENSE_FIELDS
+from src.companies.constants import (
+    TEST_ERROR_LICENSE_FIELDS,
+    title_license_id_company,
+    title_logo_company,
+    title_name_company,
+    title_slug_company,
+    title_start_license_time,
+)
 from src.companies.schemas.mixins import GetterSlugMixin
-from src.constants import LENGTH_NAME_COMPANY, MIN_LENGTH_NAME
+from src.constants import (
+    LENGTH_NAME_COMPANY,
+    LENGTH_NAME_USER,
+    LENGTH_TELEGRAM_USERNAME,
+    MIN_LENGTH_NAME,
+)
+from src.users.constants import (
+    title_name_user,
+    title_phone_number_user,
+    title_surname_user,
+    title_telegram_username_user,
+)
 
 
 class CompanyUpdateForUserSchema(BaseModel):
@@ -14,11 +31,11 @@ class CompanyUpdateForUserSchema(BaseModel):
 
     description: Optional[str] = Field(
         None,
-        title='',
+        title=title_name_company,
     )
     logo: Optional[str] = Field(
         None,
-        title='',
+        title=title_logo_company,
     )
 
 
@@ -29,15 +46,15 @@ class CompanyUpdateSchema(CompanyUpdateForUserSchema):
         None,
         min_length=MIN_LENGTH_NAME,
         max_length=LENGTH_NAME_COMPANY,
-        title='',
+        title=title_name_company,
     )
     license_id: Optional[int] = Field(
         None,
-        title='',
+        title=title_license_id_company,
     )
     start_license_time: Optional[datetime] = Field(
         None,
-        title='',
+        title=title_start_license_time,
     )
 
     @model_validator(mode='after')
@@ -61,9 +78,9 @@ class CompanyCreateSchema(GetterSlugMixin, CompanyUpdateSchema):
         ...,
         min_length=MIN_LENGTH_NAME,
         max_length=LENGTH_NAME_COMPANY,
-        title='',
+        title=title_name_company,
     )
-    slug: str = Field(..., title='')
+    slug: str = Field(..., title=title_slug_company)
 
 
 class CompanyResponseSchema(BaseModel):
@@ -86,8 +103,41 @@ class CompanyResponseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
-class CompanyResponseForUserSchema(CompanyResponseSchema):
-    """Схема компании для ответов пользователям."""
+class UserCompanyUpdateSchema(BaseModel):
+    """Схема для редактирования пользователем компании своего профиля."""
 
-    # TODO: Обдумать о необходимости отдельной схемы - чего скрывать то?
-    pass
+    name: Optional[str] = Field(
+        None,
+        min_length=MIN_LENGTH_NAME,
+        max_length=LENGTH_NAME_USER,
+        title=title_name_user,
+    )
+    surname: Optional[str] = Field(
+        None,
+        min_length=MIN_LENGTH_NAME,
+        max_length=LENGTH_NAME_USER,
+        title=title_surname_user,
+    )
+    phone_number: Optional[str] = Field(
+        None,
+        min_length=MIN_LENGTH_NAME,
+        max_length=LENGTH_NAME_USER,
+        title=title_phone_number_user,
+    )
+    email: Optional[EmailStr]
+    telegram_username: Optional[str] = Field(
+        None,
+        max_length=LENGTH_TELEGRAM_USERNAME,
+        title=title_telegram_username_user,
+    )
+
+
+class CompanyFeedbackCreateShema(BaseModel):
+    """Схема для создания пользователем компании обратной связи."""
+
+    question: str = Field(..., title='Задать вопрос для обратной связи')
+    # TODO: Обдумать. Скорее всего надо будет реализовать ограничение на количество символов.
+    # Схема на данный момент является по большей части заглушкой.
+
+    class Config:
+        from_attributes = True
