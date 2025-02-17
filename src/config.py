993b8b247c01febaf3b508dee0a6e4604f2e4c81
AndreiAgeev <ageevandrei1996@gmail.com>
2025-02-17 14:37:56 +0300

    added basic configuration, not finished

--- src/config.py
+++ src/config.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
             f'/{self.postgres_db}'
         )
 
-    model_config = ConfigDict(env_file='.env')
+    model_config = ConfigDict(env_file='.env', extra='ignore')
 
 
 settings = Settings()
