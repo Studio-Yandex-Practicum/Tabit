06da9804fdb233b8cddbdd76b1fcadd027e6d880
MrScroogeMcDuk <152524860+MrScroogeMcDuk@users.noreply.github.com>
2025-02-20 00:17:30 +0300

    Feature/crud and schems problems 136 137 (#150)
    1. Обновлена 01 миграция (Исправлено существоание 2 первичных ключей в
    модели Meeting (problem_id теперь не первичный ключ), Добавлена
    автогенерация id связных таблиц AssociationUserMeeting,
    AssociationUserProblem для правильной работы).
    2. Встречи и Проблемы теперь создаются с участниками (их можно
    добавлять), отсальыне круг операции осталсиь без изменений.
    3. Для упрощения работы с добавлением участников была создан
    универсальный файл src/problem/cred/association_utils.
    4. Проблемы и встречи можно редактировать, но не участников встречи
    "пока что", просматривать по ним инфу.

--- src/api/v1/endpoints/problem_meetings.py
+++ src/api/v1/endpoints/problem_meetings.py
@@ -1,45 +1,85 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db_depends import get_async_session
+from src.problems.crud.meeting import meeting_crud
+from src.problems.schemas.meeting import (
+    MeetingCreateSchema,
+    MeetingResponseSchema,
+    MeetingUpdateSchema,
+)
 
 router = APIRouter()
 
 
 @router.get(
     '/{company_slug}/problems/{problem_id}/meetings',
+    response_model=list[MeetingResponseSchema],
+    response_model_exclude_none=True,
     summary='Получить список всех встреч',
-    dependencies=[Depends(get_async_session)],
+    status_code=status.HTTP_200_OK,
 )
 async def meetings(
     company_slug: str,
     problem_id: int,
     session: AsyncSession = Depends(get_async_session),
 ):
-    """Получение списка всех встреч."""
-    return {'message': 'Список встреч пока пуст'}
+    """Получает список всех встреч.
+
+    Назначение:
+        Возвращает список всех встреч для указанной проблемы.
+    Параметры:
+        company_slug: Уникальный идентификатор компании.
+        problem_id: Идентификатор проблемы.
+        session: Асинхронная сессия SQLAlchemy.
+    Возвращаемое значение:
+        Список объектов MeetingResponseSchema.
+    """
+    filters = {'problem_id': problem_id}
+    return await meeting_crud.get_multi(session, filters=filters)
 
 
 @router.post(
     '/{company_slug}/problems/{problem_id}/meetings',
+    response_model=MeetingResponseSchema,
+    response_model_exclude_none=True,
     summary='Создать встречу',
-    dependencies=[Depends(get_async_session)],
+    status_code=status.HTTP_201_CREATED,
 )
 async def create_meeting(
+    meeting: MeetingCreateSchema,
     company_slug: str,
     problem_id: int,
     session: AsyncSession = Depends(get_async_session),
 ):
-    """Создать встречу."""
+    """Создает встречу.
+
+    Назначение:
+        Создает новую встречу для указанной проблемы.
+    Параметры:
+        meeting: Данные для создания встречи.
+        company_slug: Уникальный идентификатор компании.
+        problem_id: Идентификатор проблемы.
+        session: Асинхронная сессия SQLAlchemy.
+    Возвращаемое значение:
+        Объект MeetingResponseSchema.
+    """
     # TODO: Проверить уникальность названия встречи
     # TODO: Проверить доступность даты встречи?
-    return {'message': 'Создание встречи пока недоступно'}
+    meeting_data = meeting.model_dump()
+    members = meeting.members or []
+    created_meeting = await meeting_crud.create_with_members(
+        session=session, meeting_data=meeting_data, members=members
+    )
+    return created_meeting
 
 
 @router.get(
     '/{company_slug}/problems/{problem_id}/meetings/{meeting_id}',
+    response_model=MeetingResponseSchema,
+    response_model_exclude_none=True,
     summary='Получить информацию о встрече',
-    dependencies=[Depends(get_async_session)],
+    status_code=status.HTTP_200_OK,
 )
 async def get_meeting(
     company_slug: str,
@@ -47,33 +87,58 @@ async def get_meeting(
     meeting_id: int,
     session: AsyncSession = Depends(get_async_session),
 ):
-    """Получение информации о встрече."""
-    # TODO: Проверить существование компании + проблемы + встречи
-    return {'message': 'Информация о встрече пока недоступна'}
+    """Получает информацию о встрече.
+
+    Назначение:
+        Возвращает информацию о конкретной встрече.
+    Параметры:
+        company_slug: Уникальный идентификатор компании.
+        problem_id: Идентификатор проблемы.
+        meeting_id: Идентификатор встречи.
+        session: Асинхронная сессия SQLAlchemy.
+    Возвращаемое значение:
+        Объект MeetingResponseSchema.
+    """
+
+    return await meeting_crud.get_or_404(session, meeting_id)
 
 
 @router.patch(
     '/{company_slug}/problems/{problem_id}/meetings/{meeting_id}',
+    response_model=MeetingResponseSchema,
+    response_model_exclude_none=True,
     summary='Обновить информацию о встрече',
-    dependencies=[Depends(get_async_session)],
+    status_code=status.HTTP_200_OK,
 )
 async def update_meeting(
+    meeting: MeetingUpdateSchema,
     company_slug: str,
     problem_id: int,
     meeting_id: int,
     session: AsyncSession = Depends(get_async_session),
 ):
-    """Обновление встречи."""
-    # TODO: Проверить существование компании + проблемы + встречи
+    """Обновляет информацию о встрече.
+
+    Назначение:
+        Обновляет данные конкретной встречи.
+    Параметры:
+        meeting: Данные для обновления встречи.
+        company_slug: Уникальный идентификатор компании.
+        problem_id: Идентификатор проблемы.
+        meeting_id: Идентификатор встречи.
+        session: Асинхронная сессия SQLAlchemy.
+    Возвращаемое значение:
+        Объект MeetingResponseSchema.
+    """
     # TODO: Проверить уникальность названия встречи
     # TODO: Проверить доступность даты встречи?
-    return {'message': 'Обновление встречи пока недоступно'}
+    return await meeting_crud.update_meeting(session, meeting_id, meeting.model_dump())
 
 
 @router.delete(
     '/{company_slug}/problems/{problem_id}/meetings/{meeting_id}',
     summary='Удалить встречу',
-    dependencies=[Depends(get_async_session)],
+    status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_meeting(
     company_slug: str,
@@ -81,6 +146,17 @@ async def delete_meeting(
     meeting_id: int,
     session: AsyncSession = Depends(get_async_session),
 ):
-    """Удаление встречи."""
+    """Удаляет встречу.
+
+    Назначение:
+        Удаляет конкретную встречу.
+    Параметры:
+        company_slug: Уникальный идентификатор компании.
+        problem_id: Идентификатор проблемы.
+        meeting_id: Идентификатор встречи.
+        session: Асинхронная сессия SQLAlchemy.
+    Возвращаемое значение:
+        None.
+    """
     # TODO: Проверить существование компании + проблемы + встречи
-    return {'message': 'Удаление встречи пока недоступно'}
+    await meeting_crud.delete_meeting(session, meeting_id)
