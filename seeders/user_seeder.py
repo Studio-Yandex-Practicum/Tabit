cb508a6820b24599460fe82bef1650e03f76e602
Keleseth <shasa.keles@gmail.com>
2025-02-23 05:57:18 +0400

    git update. user_seeder fill

--- seeders/user_seeder.py
+++ seeders/user_seeder.py
@@ -1,10 +1,18 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import asyncio
 
-from src.companies.models.models import User
+from sqlalchemy import exists, select
+
+from src.companies.factories.company_factory import create_company
+from src.companies.models.models import Company
+from src.database.db_depends import get_async_session
+from src.tabit_management.schemas.admin_company import (
+    CompanyAdminCreateSchema,
+)
+from src.users.factories.company_user_factory import CompanyUserFactory, get_user_manager_instance
+from src.users.schemas.user import UserCreateSchema
 
 from .base_seeder import BaseSeeder
 from .constants import FAKER_USER_COUNT
-from .faker_instance import fake
 
 
 class UserSeeder(BaseSeeder):
@@ -19,15 +27,39 @@ class UserSeeder(BaseSeeder):
         """
         super().__init__(count)
 
-    async def run(self, session: AsyncSession):
+    async def run(self, amount: int = 1, company_id: int | None = None, is_admin: bool = False):
         """
         Генерация и добавление пользователей в базу данных.
         :param session: асинхронная сессия SQLAlchemy
         """
-        # TODO: когда будет модель, поправлю поля
-        users = [User(username=fake.user_name(), email=fake.email()) for _ in range(self.count)]
-        session.add_all(users)
-        await session.commit()
+        async for session in get_async_session():
+            try:
+                if not company_id:
+                    new_company = await create_company(session=session)
+                    company_id = new_company.id
+                else:
+                    company_exists = await session.execute(
+                        select(exists().where(Company.id == company_id))
+                    )
+                    if not company_exists.scalar():
+                        raise ValueError(f'Компания с ID {company_id} отсутствует в бд.')
 
-        # TODO: пока print, но нужно логирование
-        print(f'Создано {self.count} пользователей.')
+                user_manager = await get_user_manager_instance(session)
+                user_schema = CompanyAdminCreateSchema if is_admin else UserCreateSchema
+                for _ in range(amount):
+                    tabit_user_data = CompanyUserFactory.build(company_id=company_id)
+                    print(tabit_user_data['company_id'])
+                    tabit_user_scheme_data = user_schema(**tabit_user_data)
+                    new_tabit_user = await user_manager.create(tabit_user_scheme_data)
+                    print(f'Создан пользователь: {new_tabit_user.name}')
+            except Exception as e:
+                print(f'Ошибка: {e}')
+                await session.rollback()
+            finally:
+                await session.close()
+
+
+user_seeder = UserSeeder()
+
+if __name__ == '__main__':
+    asyncio.run(user_seeder.run(amount=1))
