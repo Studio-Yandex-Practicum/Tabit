ea43675e1ec827a2a043541b8d6fe1678db0ef49
Keleseth <shasa.keles@gmail.com>
2025-02-26 03:01:31 +0400

    argparse development started

--- fake_data_factories/__init__.py
+++ fake_data_factories/__init__.py
@@ -1,4 +1,13 @@
 # Импорты сидеров для удобства вызова
-from .company_factories import CompanyFactory
+from .company_factories import CompanyFactory, create_companies
+from .company_user_factories import CompanyUserFactory, create_company_users
+from .tabit_user_factories import TabitAdminUserFactory, create_tabit_admin_users
 
-__all__ = ['CompanyFactory']
+__all__ = [
+    'CompanyFactory',
+    'create_companies',
+    'CompanyUserFactory',
+    'create_company_users',
+    'TabitAdminUserFactory',
+    'create_tabit_admin_users',
+]
