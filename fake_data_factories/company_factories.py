c71ba2688b863fa5c536e17249b51ca526928441
Keleseth <shasa.keles@gmail.com>
2025-03-03 16:21:41 +0300

    LicenseTypeFactory and filling function were added. Also annotations for factories were fixed.

--- fake_data_factories/company_factories.py
+++ fake_data_factories/company_factories.py
@@ -4,7 +4,6 @@ from random import randint
 
 import factory
 from async_factory_boy.factory.sqlalchemy import AsyncSQLAlchemyFactory
-from pydantic import HttpUrl
 from termcolor import cprint
 
 from constants import FAKER_COMPANY_COUNT
@@ -18,23 +17,27 @@ class CompanyFactory(AsyncSQLAlchemyFactory):
     Фабрика генерации данных компании.
 
     Поля:
-        name: Faker генерированное поле.
-        description: Faker генерированное поле.
-        logo: Faker генерированное поле.
-        max_employees_count: Задается случайное значение от 1 до 10.
-        is_active: Bool значение, по умолчанию True.
-        slug: Slug генерируется из знаков названия + библиотеки uuid.
-        max_admins_count: Задается случайное значение от 1 до 5.
+        1. name: Faker генерированное поле.
+        2. description: Faker генерированное поле.
+        3. logo: Faker генерированное поле.
+        4. max_employees_count: Задается случайное значение от 1 до 10.
+        5. is_active: Bool значение, по умолчанию True.
+        6. slug: Slug генерируется из знаков названия + библиотеки uuid.
+        7. max_admins_count: Задается случайное значение от 1 до 5.
+        8. license_id: Необязательное поле - Тип лицензии компании.
     """
 
     id = None
-    name: str = factory.Faker('company', locale='ru_RU')
-    description: str = factory.Faker('text', max_nb_chars=256)
-    logo: HttpUrl = factory.Faker('image_url')
-    max_employees_count: int = factory.LazyFunction(lambda: randint(1, 10))
+    name: factory.Faker = factory.Faker('company', locale='ru_RU')
+    description: factory.Faker = factory.Faker('text', max_nb_chars=256)
+    logo: factory.Faker = factory.Faker('image_url')
+    max_employees_count: factory.LazyFunction = factory.LazyFunction(lambda: randint(1, 10))
     is_active: bool = True
-    slug: str = factory.LazyAttribute(lambda obj: f'{obj.name.lower()[:3]}_{uuid.uuid4().hex[:6]}')
-    max_admins_count: int = factory.LazyFunction(lambda: randint(20, 30))
+    slug: factory.LazyAttribute = factory.LazyAttribute(
+        lambda obj: f'{obj.name.lower()[:3]}_{uuid.uuid4().hex[:6]}'
+    )
+    max_admins_count: factory.LazyFunction = factory.LazyFunction(lambda: randint(20, 30))
+    license_id: int | None = None
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
@@ -47,11 +50,11 @@ class CompanyFactory(AsyncSQLAlchemyFactory):
         sqlalchemy_session = sc_session
 
 
-async def create_companies(count: int = FAKER_COMPANY_COUNT) -> None:
+async def create_companies(count: int = FAKER_COMPANY_COUNT, **kwargs) -> None:
     """
     Функция для наполнения таблицы бд Company.
     """
-    companies = await CompanyFactory.create_batch(count)
+    companies = await CompanyFactory.create_batch(count, **kwargs)
     cprint(f'Создано {count} Companies', 'green')
     return companies
 
