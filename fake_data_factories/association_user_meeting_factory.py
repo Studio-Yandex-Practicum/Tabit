0f5a87090511e27c5765bfc284bdf1d6610b69e8
Evgeniy Pshenko <Fox.Warlock82@yandex.ru>
2025-03-12 19:42:26 +0300

    fix

--- fake_data_factories/association_user_meeting_factory.py
+++ fake_data_factories/association_user_meeting_factory.py
@@ -6,28 +6,29 @@ from termcolor import cprint
 
 from src.database.sc_db_session import sc_session
 from src.database.alembic_models import AssociationUserMeeting
-from meeting_factory import MeetingFactory, UserTabitFactory
+from fake_data_factories.meeting_factory import MeetingFactory
+from fake_data_factories.company_user_factories import CompanyUserFactory
 
 
 class AssociationUserMeetingFactory(AsyncSQLAlchemyFactory):
 
-    left = factory.SubFactory(UserTabitFactory)
-    right = factory.SubFactory(MeetingFactory)
-    created_at = factory.LazyFunction(datetime.utcnow)
-    updated_at = factory.LazyFunction(datetime.utcnow)
+    left: factory.SubFactory = factory.SubFactory(CompanyUserFactory)
+    right: factory.SubFactory = factory.SubFactory(MeetingFactory)
+    created_at: factory.LazyFunction = factory.LazyFunction(datetime.utcnow)
+    updated_at: factory.LazyFunction = factory.LazyFunction(datetime.utcnow)
 
     class Meta:
         model = AssociationUserMeeting
         sqlalchemy_session = sc_session
 
 
-async def create_user_meeting_associations(user_id, meeting_ids):
+async def create_user_meeting_associations(users, meetings):
     """
-    Функция для создания связей между пользователем и встречами.
+    Функция для создания связей между пользователями и встречами.
     """
-    associations = [
-        await AssociationUserMeetingFactory.create(left_id=user_id, right_id=meeting_id)
-        for meeting_id in meeting_ids
-    ]
-    cprint(f'Создано {len(meeting_ids)} связей между пользователем {user_id} и встречами', 'green')
-    return associations
+
+    for meeting in meetings:
+        company_users = [user for user in users if user.company_id == meeting.owner.company_id]
+        for user in company_users[:2]:
+            await AssociationUserMeetingFactory.create(left=user, right=meeting)
+    cprint(f'Созданы ассоциации для {len(users)} пользователей', 'green')
--- fake_data_factories/meeting_factory.py
+++ fake_data_factories/meeting_factory.py
@@ -2,65 +2,71 @@ from datetime import datetime
 
 import asyncio
 import factory
+from sqlalchemy import UUID
 from async_factory_boy.factory.sqlalchemy import AsyncSQLAlchemyFactory
 from termcolor import cprint
 
 from src.logger import fake_db_logger
 from src.database.sc_db_session import sc_session
 from src.database.annotations import int_zero
-from src.database.alembic_models import Meeting, UserTabit, Problem
-from constants import FAKER_MEETING_COUNT
+from src.database.alembic_models import Meeting, Problem
+from src.problems.models.enums import StatusMeeting
+from constants import FAKER_MEETING_COUNT, FAKER_USER_COUNT
 
 
 class ProblemFactory(AsyncSQLAlchemyFactory):
 
-    title: factory.Faker = factory.Faker('sentence')
+    name: factory.Faker = factory.Faker('sentence')
     description: factory.Faker = factory.Faker('text')
+    company_id: int = factory.LazyAttribute(lambda obj: getattr(obj, 'company_id', None))
 
     class Meta:
         model = Problem
         sqlalchemy_session = sc_session
 
 
-class UserTabitFactory(AsyncSQLAlchemyFactory):
-
-    username: factory.Faker = factory.Faker('user_name')
-    email: factory.Faker = factory.Faker('email')
-
-    class Meta:
-        model = UserTabit
-        sqlalchemy_session = sc_session
-
-
 class MeetingFactory(AsyncSQLAlchemyFactory):
 
-    title: factory.Faker = factory.Faker('Meeting', locale='ru_RU')
+    title: factory.Faker = factory.Faker('sentence', locale='ru_RU')
     description: factory.Faker = factory.Faker('text', max_nb_chars=255)
-    problem: factory.SubFactory = factory.SubFactory(ProblemFactory)
-    owner: factory.SubFactory = factory.SubFactory(UserTabitFactory)
+    company_id
+    problem = factory.SubFactory(ProblemFactory, company_id=factory.SelfAttribute('..company_id'))
+    owner_id: UUID
     date_meeting: factory.Faker = factory.Faker('future_datetime')
-    status: factory.Iterator = factory.Iterator(['scheduled', 'canceled', 'completed'])
+    status: factory.Iterator = factory.Iterator([
+        StatusMeeting.NEW, StatusMeeting.NOT_HELD,
+        StatusMeeting.HELD, StatusMeeting.SUSPENDED
+    ])
     place: factory.Faker = factory.Faker('address')
     transfer_counter: int_zero = 0
     created_at: factory.LazyFunction = factory.LazyFunction(datetime.utcnow)
     updated_at: factory.LazyFunction = factory.LazyFunction(datetime.utcnow)
-
-    @classmethod
-    def _create(cls, model_class, *args, **kwargs):
-        instance = super()._create(model_class, *args, **kwargs)
-        fake_db_logger.info(f'Создана встреча {kwargs.get("title")}')
-        return instance
 
     class Meta:
         model = Meeting
         sqlalchemy_session = sc_session
 
 
-async def create_meetings(count: int = FAKER_MEETING_COUNT, **kwargs) -> None:
-    meetings = await MeetingFactory.create_batch(count, **kwargs)
-    cprint(f'Создано {count} Meetengs', 'green')
+async def create_meetings(count: int = FAKER_MEETING_COUNT, **kwargs):
+    from fake_data_factories.association_user_meeting_factory import create_user_meeting_associations
+    meetings = await MeetingFactory.create_batch(count, company_id=company_id, **kwargs)
+    cprint(f'Создано {count} Meetings', 'green')
+    await create_user_meeting_associations(users, meetings)
     return meetings
 
 
+async def main():
+    from fake_data_factories.company_factories import CompanyFactory
+    from fake_data_factories.company_user_factories import CompanyUserFactory
+
+    company = await CompanyFactory.create()
+    kwargs['company_id'] = company.id
+    users = []
+    kwargs['users'] = users
+    for _ in range(FAKER_USER_COUNT):
+        user = await CompanyUserFactory.create(**kwargs)
+        users.append(user)
+    await create_meetings(count=FAKER_MEETING_COUNT, **kwargs)
+
 if __name__ == '__main__':
-    asyncio.run(create_meetings())
+    asyncio.run(main())
