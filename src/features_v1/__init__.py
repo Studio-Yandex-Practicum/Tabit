03fe877d6a6a02cd12b46f348ef54f5e814062f4
Andrey Zherdetskiy <aszherdetskiy@gmail.com>
2025-03-13 15:26:35 +0400

    Fix constants, routers, admin_user_crud. Pass tests

--- src/features_v1/__init__.py
+++ src/features_v1/__init__.py
@@ -5,7 +5,6 @@ from .company_problem_meetings.endpoints import router as company_problem_meetin
 from .company_problem_tasks.endpoints import router as company_problem_tasks_router
 from .company_survey_management.endpoints import router as company_survey_management_router
 from .company_user_auth.endpoints import router as company_user_auth_router
-from .company_user_profile.endpoints import router as company_user_profile_router
 from .landing_page.endpoints import router as landing_page_router
 from .tabit_admin_auth.endpoints import router as tabit_admin_auth_router
 from .tabit_admin_management.endpoints import router as tabit_admin_management_router
