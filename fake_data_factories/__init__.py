301e3038c6757334002e7aa4233ac01f21897625
Arch <soulyraziel@gmail.com>
2025-03-30 21:30:27 +0300

    Импорты в fake_data_factories/__init__.py

--- fake_data_factories/__init__.py
+++ fake_data_factories/__init__.py
@@ -3,6 +3,7 @@ from .association_user_problem_factory import (
     AssociationUserProblemFactory,
     create_user_problem_associations,
 )
+from .association_user_tags_factory import AssociationUserTagsFactory, create_user_tag_associations
 from .association_user_task_factory import (
     AssociationUserTaskFactory,
     create_user_task_associations,
@@ -15,6 +16,7 @@ from .license_type_factories import LicenseTypeFactory, create_license_type
 from .message_feed_factory import MessageFeedFactory, create_message_feeds
 from .problem_factory import ProblemFactory, create_problems
 from .tabit_user_factories import TabitAdminUserFactory, create_tabit_admin_users
+from .tag_factories import TagUserFactory, create_tags
 from .task_factory import TaskFactory, create_tasks
 from .voting_feed_factory import VotingFeedFactory, create_voting_feeds
 
@@ -34,12 +36,16 @@ __all__ = [
     'create_problems',
     'TabitAdminUserFactory',
     'create_tabit_admin_users',
+    'TagUserFactory',
+    'create_tags',
     'TaskFactory',
     'create_tasks',
     'VotingFeedFactory',
     'create_voting_feeds',
     'AssociationUserProblemFactory',
     'create_user_problem_associations',
+    'AssociationUserTagsFactory',
+    'create_user_tag_associations',
     'AssociationUserTaskFactory',
     'create_user_task_associations',
 ]
