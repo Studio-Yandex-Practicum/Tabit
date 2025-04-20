ba05c86a44240f6cc04ce6a21df28f2ef0211db3
Alexey Sudakov <outzz@yandex.ru>
2025-04-20 22:47:01 +0300

    Add: Surveys класс с методами для расчета результатов

---
+++ src/utils/surveys.py
@@ -0,0 +1,21 @@
+from src.schemas.survey import SurveyAnswer
+
+
+class Surveys:
+    def __init__(self, item: SurveyAnswer):
+        self.item = item
+
+    def survey_type(self):
+        match self.item.survey_list_id:
+            case 1:
+                return self.lusher()
+            case 2:
+                return self.etkind()
+
+    def lusher(self):
+        if self.item.answers == [1, 2, 3, 4, 5, 6]:
+            return {'result': 'good'}
+        return {'result': 'bad'}
+
+    def etkind(self):
+        return {'result': 'bad'}
