626f44aa7c38092376a5f8ec823dd18001b90f96
Alexey Sudakov <outzz@yandex.ru>
2025-04-22 21:36:54 +0300

    Add: добавлены докстринги к схемам

--- src/schemas/survey.py
+++ src/schemas/survey.py
@@ -1,21 +1,50 @@
 from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class SurveyScheduleCycleCreate(BaseModel):
-    cycle_number: int
+    """
+    Схема создания циклов в расписании тестирований.
+
+    Назначение:
+        Определяет структуру данных для создания циклов в расписании.
+    Параметры:
+        cycle_number: номер цикла в расписании (6 циклов максимум).
+        date_start: дата начала тестирования.
+    """
+    cycle_number: int = Field(..., ge=1, le=6)
     date_start: datetime
 
 
 class SurveyScheduleCycleRead(SurveyScheduleCycleCreate):
+    """
+    Схема получения циклов в расписании.
+
+    Назначение:
+        Определяет структуру ответа получения циклов внутри расписания.
+    Параметры:
+        id: Идентификатор цикла
+        cycle_number: номер цикла в расписании (6 циклов максимум).
+        date_start: дата начала тестирования.
+    """
     id: int
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class SurveyScheduleCreate(BaseModel):
+    """
+    Схема создания расписания.
+    
+    Назначение:
+        Определяет структуру запроса для создания расписания.
+    Параметры:
+        survey_tag: Таг определяетщий вид тестирования.
+        status: текущий статус расписания.
+        cycles: список циклов тестирований.
+    """
     survey_tag: str
     status: str
     cycles: List[SurveyScheduleCycleCreate]
@@ -40,6 +69,17 @@ class SurveyScheduleCreate(BaseModel):
 
 
 class SurveyScheduleRead(BaseModel):
+    """
+    Схема получения расписания.
+
+    Назначение:
+        Определяет структуру данных для ответа с информацией об расписании.
+    Параметры:
+        id: Идентификатор
+        survey_tag: Таг определяетщий вид тестирования.
+        created_at: Дата создания расписания.
+        status: текущий статус расписания.
+    """
     id: int
     survey_tag: str
     created_at: datetime
@@ -49,11 +89,30 @@ class SurveyScheduleRead(BaseModel):
 
 
 class SurveyAnswerCreate(BaseModel):
+    """
+    Схема создания ответов.
+
+    Назначение:
+        Определяет структуру запроса на создание ответов тестов.
+    Параметры:
+        survey_list_id: Уникальный идентификатор теста.
+        answers: Список ответов.
+    """
     survey_list_id: int
     answers: List[int]
 
 
 class SurveyDataCreate(BaseModel):
+    """
+    Схема создания данных об прохождении тестов.
+
+    Назначение:
+        Определяет структуру запроса на создание данных об прохождении теста сотрудника.
+    Параметры:
+        survey_shedule_id: Идентификатор расписания к которому привязаны данные.
+        cycle_id: Идентификатор цикла к которому привязаны данные.
+        answers: Список ответов.
+    """
     survey_shedule_id: int
     cycle_id: int
     answers: List[SurveyAnswerCreate]
@@ -82,6 +141,16 @@ class SurveyDataCreate(BaseModel):
 
 
 class SurveyDataRead(BaseModel):
+    """
+    Схема получения данных об прохождении тестов.
+
+    Назначение:
+        Определяет структуру данных для ответа на запрос данных о прохождении теста.
+    Параметры:
+        id: Идентификатор записи.
+        cycle_id: Идентификатор цикла к которому привязаны данные.
+        results: Окончательный результат тестирования на основе всех тестов.
+    """
     id: int
     cycle_id: int
     results: Optional[dict]
