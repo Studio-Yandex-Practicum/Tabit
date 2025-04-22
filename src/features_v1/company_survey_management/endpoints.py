1b0932ac593468423aa524cfcfbe1daa1be610d1
Alexey Sudakov <outzz@yandex.ru>
2025-04-22 22:02:17 +0300

    Add: добавлены description.

--- src/features_v1/company_survey_management/endpoints.py
+++ src/features_v1/company_survey_management/endpoints.py
@@ -24,10 +24,16 @@ router = APIRouter()
     '/',
     response_model=List[SurveyScheduleRead],
     summary='Получить список всех опросов компании',
+    description='Получить список всех опросов компании. Права доступа: Tabit Admin, Tabit Superuser.',
     dependencies=[Depends(get_async_session)],
 )
 async def get_surveys(company_slug: str, session: AsyncSession = Depends(get_async_session)):
-    """Получает список всех опросов компании."""
+    """
+    Возвращает список всех опросов компании.
+
+    Назначение:
+        Для получения списка всех расписаний проводившихся опросов.
+    """
     await validator_check_object_exists(
         session=session, model_crud=company_crud, object_slug=company_slug
     )
@@ -44,6 +50,7 @@ async def get_surveys(company_slug: str, session: AsyncSession = Depends(get_asy
     '/',
     response_model=SurveyScheduleRead,
     summary='Создать новое расписание опросов',
+    description='Создать новое расписание опросов. Права доступа: Tabit Admin, Tabit Superuser.',
     dependencies=[Depends(get_async_session)],
 )
 async def create_survey_schedule(
@@ -55,13 +62,13 @@ async def create_survey_schedule(
     Создает новое расписание.
 
     Поля:
-        date_start: заполняется в формате "2019-08-24".
-        status: IN_PROGRESS = 'В работе'
-                COMPLETED = 'Завершен'
-                CANCELED = 'Отменен'
-                POSTPONED = 'Отложен'
-        survey_tag: EMO = 'Определение эмоционального состояния'
-                    TEST = 'Тестовый тест для тестирования'
+     - date_start: заполняется в формате "2019-08-24".
+     - status: IN_PROGRESS = 'В работе'
+               COMPLETED = 'Завершен'
+               CANCELED = 'Отменен'
+               POSTPONED = 'Отложен'
+     - survey_tag: EMO = 'Определение эмоционального состояния'
+                   TEST = 'Тестовый тест для тестирования'
     """
     await validator_check_object_exists(
         session=session, model_crud=company_crud, object_slug=company_slug
@@ -77,6 +84,7 @@ async def create_survey_schedule(
     '/{schedule_id:int}',
     response_model=SurveyScheduleRead,
     summary='Удалить расписание опросов',
+    description='Позволяет удалить расписание опросов. Права доступа: Tabit Admin, Tabit Superuser',
     dependencies=[Depends(get_async_session)],
 )
 async def delete_survey_schedule(
@@ -102,6 +110,7 @@ async def delete_survey_schedule(
     '/{schedule_id:int}/cycles',
     response_model=List[SurveyScheduleCycleRead],
     summary='Получить список циклов опросов внутри расписания компании',
+    description='Позволяет получить список циклов опросов внутри конкретного расписания. Права доступа: Tabit Admin, Tabit Superuser',
     dependencies=[Depends(get_async_session)],
 )
 async def get_surveys_cycles(
@@ -122,6 +131,7 @@ async def get_surveys_cycles(
     '/{user_id:uuid}',
     response_model=List[SurveyDataRead],
     summary='Получить историю опросов сотрудника компании',
+    description='Позволяет получить испорию всех пройденых опросов пользователя. Права доступа: Company User.',
     dependencies=[Depends(get_async_session)],
 )
 async def get_employee_survey_history(
@@ -145,6 +155,7 @@ async def get_employee_survey_history(
     '/{user_id}/{survey_id}',
     response_model=SurveyDataRead,
     summary='Получить информацию об опросе сотрудника компании',
+    description='Позволяет получить информацию о конкретном опросе пользователя. Права доступа: Company User.',
     dependencies=[Depends(get_async_session)],
 )
 async def get_employee_survey_info(
@@ -170,6 +181,7 @@ async def get_employee_survey_info(
 @router.post(
     '/{user_id}',
     summary='Передать информацию об опросе сотрудника компании',
+    description='Позволяет передать данные о прохождении опроса пользователем. Права доступа: Company User.',
     dependencies=[Depends(get_async_session)],
 )
 async def add_employee_survey_info(
