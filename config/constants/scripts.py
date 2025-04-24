bb17d27eeb7c368a74f7aa47e08225bc86d64bb6
Evgeny Shutov <acidic@ya.ru>
2025-04-24 20:04:24 +0300

    Fixed some bugs

--- config/constants/scripts.py
+++ config/constants/scripts.py
@@ -31,7 +31,7 @@ class MigrationConstants:
             Сопоставляет цифры в начале имени файла перед '_'.
     """
 
-    MIGRATIONS_DIR: Path = Path(__file__).resolve().parent.parent.parent / 'alembic' / 'versions'
+    MIGRATIONS_DIR: Path = Path(__file__).resolve().parents[2] / 'alembic' / 'versions'
     MIGRATION_RE_ID: Pattern[str] = compile(r'^(\d+)_')
 
 
