a71b11fb8bcce4ae62626beed3237ca7bbaa648d
Alexey Sudakov <outzz@yandex.ru>
2025-04-29 23:06:05 +0300

    Add: surveys cruds

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -13,7 +13,7 @@ from .crud_problem import problem_crud
 from .crud_task import task_crud
 from .crud_user import user_crud
 from .crud_meeting_result import result_meeting_crud
-from .crud_surveys import surveys_schedule_crud
+from .crud_surveys import surveys_schedule_crud, surveys_data_crud, surveys_list_crud
 
 __all__ = [
     'user_comment_association_crud',
@@ -34,4 +34,6 @@ __all__ = [
     'task_crud',
     'user_crud',
     'surveys_schedule_crud',
+    'surveys_data_crud',
+    'surveys_list_crud',
 ]
